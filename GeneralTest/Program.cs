ac93f6f15dc7e4b1a0f9db3d056963e03fce683e
yspxman <yspxman@gmail.com>
2012-07-17 17:25:20 +0800

    add GeneralTest.csproj

---
+++ GeneralTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneralTest
+{
+
+    //delegate是声明了一种类型而不是变量，event是变量， 类型是该Delegate
+    public delegate string MyDelegate(int num);
+
+    public interface Iface
+    {
+        void dispose();
+        event EventHandler Event1;
+        event MyDelegate Event2;
+        int member1 { get; set; }
+        
+        // interface 可以包含以上三种，但不能有成员变量
+        //int member2; 
+    }
+
+    public class MyFace : Iface
+    {
+       public void dispose(){;}
+       public event EventHandler Event1;
+       public event MyDelegate Event2;
+       public int member1 { get; set; }
+        
+    }
+
+    public class MyFace2 : MyFace
+    {
+
+        //EventHandler<
+        public void InvokeEvent()
+        {
+            // event 没有继承下来，下面的语句编译不通过
+            //if (Event1 != null){}
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
